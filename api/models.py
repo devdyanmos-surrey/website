5bccc3bc6d3978b0f39d1cfeecb20bee0cfd9222
choolwe <chichachoolwe@gmail.com>
2024-02-29 17:55:09 +0000

    Testing Commit to New branch

--- api/models.py
+++ api/models.py
@@ -8,7 +8,7 @@ class Student(models.Model):
     name = models.CharField(max_length=100)
     age = models.IntegerField()
     # academic = models.ForeignKey('Academic', on_delete=models.CASCADE)
-    URL = models.CharField(max_length=10, unique=True)
+    URN = models.CharField(max_length=10, primary_key=True)
     password = models.CharField(max_length=100, null=False)
 
     def __str__(self):
@@ -17,3 +17,12 @@ class Student(models.Model):
     # create a method to check if the password is valid 
     def is_valid(self, password):
         return self.password == password
+
+class Project(models.Model):
+    project_ID = models.IntegerField(primary_key=True)
+    project_details = models.CharField(max=100)
+    
+
+class Student_Project(models.Model):
+    urn = models.IntegerField(primary_key=True)
+    project_ID = models.ForeignKey('Projects', on_delete=models.CASCADE)
